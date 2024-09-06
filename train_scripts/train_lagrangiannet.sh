25ce4edffbee8da68bbe5c8290da7fceeae38b5c
molumitu <zgj21@mails.tsinghua.edu.cn>
2024-09-06 09:23:35 +0800

    upload parallel training sh scripts

---
+++ train_scripts/train_lagrangiannet.sh
@@ -0,0 +1,11 @@
+python ../example_train/fhadp_lagrangiannet/fhadp_lagrangiannet_mlp_veh3dof_tracking_detour_serial.py \
+    --pre_horizon 10 --eval_accuracy True \
+& sleep 5
+python ../example_train/fhadp_lagrangiannet/fhadp_lagrangiannet_mlp_veh3dof_tracking_detour_serial.py \
+    --pre_horizon 20 --eval_accuracy True \
+& sleep 5
+python ../example_train/fhadp_lagrangiannet/fhadp_lagrangiannet_mlp_veh3dof_tracking_detour_serial.py \
+    --pre_horizon 30 --eval_accuracy True \
+& sleep 5
+python ../example_train/fhadp_lagrangiannet/fhadp_lagrangiannet_mlp_veh3dof_tracking_detour_serial.py \
+    --pre_horizon 40 --eval_accuracy True \
