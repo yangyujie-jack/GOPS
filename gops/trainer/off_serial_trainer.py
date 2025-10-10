7003768a941c48ae2ad58d6e3e06cf29c7cf1734
yangyujie <yangyj21@mails.tsinghua.edu.cn>
2025-10-10 18:17:42 +0800

    remove unnecessary eval task pool

--- gops/trainer/off_serial_trainer.py
+++ gops/trainer/off_serial_trainer.py
@@ -13,9 +13,10 @@
 
 __all__ = ["OffSerialTrainer"]
 
-from cmath import inf
 import os
 import time
+from cmath import inf
+from tqdm import tqdm
 
 import ray
 import torch
@@ -23,7 +24,6 @@ from torch.utils.tensorboard import SummaryWriter
 
 from gops.trainer.evaluator import Evaluator
 from gops.utils.common_utils import ModuleOnDevice
-from gops.utils.parallel_task_manager import TaskPool
 from gops.utils.tensorboard_setup import add_scalars, tb_tags
 from gops.utils.log_data import LogData
 
@@ -54,21 +54,16 @@ class OffSerialTrainer:
         self.save_folder = kwargs["save_folder"]
         self.iteration = 0
 
+        self.sampler_tb_dict = LogData()
+        self.alg_tb_dict = LogData()
         self.writer = SummaryWriter(log_dir=self.save_folder, flush_secs=20)
-        # flush tensorboard at the beginning
-        add_scalars(
-            {tb_tags["alg_time"]: 0, tb_tags["sampler_time"]: 0}, self.writer, 0
-        )
-        self.writer.flush()
 
         # pre sampling
         while self.buffer.size < kwargs["buffer_warm_size"]:
             samples, _ = self.sampler.sample()
             self.buffer.add_batch(samples)
-        self.sampler_tb_dict = LogData()
 
-        # create evaluation tasks
-        self.evaluate_tasks = TaskPool()
+        self.eval_task_id = None
         self.last_eval_iteration = 0
 
         self.use_gpu = kwargs["use_gpu"]
@@ -102,13 +97,13 @@ class OffSerialTrainer:
         else:
             alg_tb_dict = self.alg.local_update(replay_samples, self.iteration)
         self.networks.eval()
+        self.alg_tb_dict.add_average(alg_tb_dict)
 
         self.iteration += 1
 
         # log
         if self.iteration % self.log_save_interval == 0:
-            print("Iter =", self.iteration)
-            add_scalars(alg_tb_dict, self.writer, step=self.iteration)
+            add_scalars(self.alg_tb_dict.pop(), self.writer, step=self.iteration)
             add_scalars(self.sampler_tb_dict.pop(), self.writer, step=self.iteration)
 
         # save
@@ -117,26 +112,18 @@ class OffSerialTrainer:
 
         # evaluate
         if self.iteration % self.eval_interval == 0:
-            if self.evaluate_tasks.count == 0:
-                # There is no evaluation task, add one.
-                self._add_eval_task()
-            else:
-                # Evaluation tasks is completed, log data and add another one.
-                objID = next(self.evaluate_tasks.completed(blocking_wait=True, timeout=None))[1]
-                self._log_eval_result(ray.get(objID))
-                self._add_eval_task()
+            if self.eval_task_id is not None:
+                self._log_eval_result(ray.get(self.eval_task_id))
+            self.evaluator.load_state_dict.remote(self.networks.state_dict())
+            self.eval_task_id = self.evaluator.run_evaluation.remote(self.iteration)
+            self.last_eval_iteration = self.iteration
 
     def train(self):
-        while self.iteration < self.max_iteration:
+        for _ in tqdm(range(self.max_iteration), desc="Training"):
             self.step()
 
-        if self.iteration % self.apprfunc_save_interval == 0:
-            self.save_apprfunc()
-
-        if self.evaluate_tasks.count == 1:
-            objID = next(self.evaluate_tasks.completed(blocking_wait=True, timeout=None))[1]
-            self._log_eval_result(ray.get(objID))
-
+        self.save_apprfunc()
+        self._log_eval_result(ray.get(self.eval_task_id))
         self.writer.flush()
 
     def save_apprfunc(self):
@@ -144,15 +131,6 @@ class OffSerialTrainer:
             self.networks.state_dict(),
             self.save_folder + "/apprfunc/apprfunc_{}.pkl".format(self.iteration),
         )
-
-    def _add_eval_task(self):
-        with ModuleOnDevice(self.networks, "cpu"):
-            self.evaluator.load_state_dict.remote(self.networks.state_dict())
-        self.evaluate_tasks.add(
-            self.evaluator,
-            self.evaluator.run_evaluation.remote(self.iteration)
-        )
-        self.last_eval_iteration = self.iteration
 
     def _log_eval_result(self, eval_result):
         constraint = isinstance(eval_result, tuple)
@@ -167,7 +145,6 @@ class OffSerialTrainer:
             and self.last_eval_iteration >= self.max_iteration / 5
         ):
             self.best_tar = total_avg_return
-            print("Best return = {}!".format(str(self.best_tar)))
 
             for filename in os.listdir(self.save_folder + "/apprfunc/"):
                 if filename.endswith("_opt.pkl"):
