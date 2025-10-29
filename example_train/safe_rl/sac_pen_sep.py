faf0f07c6099fe676481f1765714dd5cb09643e1
yangyujie <yangyj21@mails.tsinghua.edu.cn>
2025-10-29 14:01:38 +0800

    add penalty separate

---
+++ example_train/safe_rl/sac_pen_sep.py
@@ -0,0 +1,86 @@
+#  Copyright (c). All Rights Reserved.
+#  General Optimal control Problem Solver (GOPS)
+#  Intelligent Driving Lab(iDLab), Tsinghua University
+#
+#  Creator: iDLab
+#  Lab Leader: Prof. Shengbo Eben Li
+#  Email: lisb04@gmail.com
+
+import argparse
+
+from gops.create_pkg.create_alg import create_alg
+from gops.create_pkg.create_buffer import create_buffer
+from gops.create_pkg.create_env import create_env
+from gops.create_pkg.create_evaluator import create_evaluator
+from gops.create_pkg.create_sampler import create_sampler
+from gops.create_pkg.create_trainer import create_trainer
+from gops.utils.init_args import init_args
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    # Key Parameters
+    parser.add_argument("--env_id", type=str, default="safe_rl")
+    parser.add_argument("--safe_rl_env_id", type=str, default="Safety-Gymnasium-SafetyPointGoal1-v0")
+    parser.add_argument("--algorithm", type=str, default="SACPenSep")
+    parser.add_argument("--enable_cuda", action="store_true")
+    parser.add_argument("--seed", type=int, default=1)
+
+    # Parameters of approximate function
+    parser.add_argument("--value_func_name", type=str, default="ActionValue")
+    parser.add_argument("--value_func_type", type=str, default="MLP")
+    parser.add_argument("--value_hidden_sizes", type=list, default=[256, 256])
+    parser.add_argument("--value_hidden_activation", type=str, default="relu")
+    parser.add_argument("--feasibility_func_name", type=str, default="ActionValue")
+    parser.add_argument("--feasibility_func_type", type=str, default="MLP")
+    parser.add_argument("--feasibility_hidden_sizes", type=list, default=[256, 256])
+    parser.add_argument("--feasibility_hidden_activation", type=str, default="relu")
+    parser.add_argument("--policy_func_name", type=str, default="StochaPolicy")
+    parser.add_argument("--policy_func_type", type=str, default="MLP")
+    parser.add_argument("--policy_act_distribution", type=str, default="TanhGaussDistribution")
+    parser.add_argument("--policy_hidden_sizes", type=list, default=[256, 256])
+    parser.add_argument("--policy_hidden_activation", type=str, default="relu")
+    parser.add_argument("--policy_min_log_std", type=float, default=-20.)
+    parser.add_argument("--policy_max_log_std", type=float, default=2.)
+
+    # Parameters for RL algorithm
+    parser.add_argument("--value_learning_rate", type=float, default=0.0001)
+    parser.add_argument("--feasibility_learning_rate", type=float, default=0.0001)
+    parser.add_argument("--policy_learning_rate", type=float, default=0.0001)
+    parser.add_argument("--alpha_learning_rate", type=float, default=0.0001)
+    parser.add_argument("--penalty", type=float, default=1.)
+
+    # Parameters for trainer
+    parser.add_argument("--trainer", type=str, default="off_serial_trainer")
+    parser.add_argument("--max_iteration", type=int, default=1000000)
+    parser.add_argument("--buffer_name", type=str, default="replay_buffer")
+    parser.add_argument("--buffer_warm_size", type=int, default=10000)
+    parser.add_argument("--buffer_max_size", type=int, default=1000000)
+    parser.add_argument("--replay_batch_size", type=int, default=256)
+    parser.add_argument("--sample_interval", type=int, default=100)
+    parser.add_argument("--apprfunc_save_interval", type=int, default=1000000)
+    parser.add_argument("--log_save_interval", type=int, default=10000)
+
+    # Parameters for sampler
+    parser.add_argument("--sampler_name", type=str, default="off_sampler")
+    parser.add_argument("--sample_batch_size", type=int, default=100)
+
+    # Parameters for evaluator
+    parser.add_argument("--evaluator_name", type=str, default="evaluator")
+    parser.add_argument("--num_eval_episode", type=int, default=10)
+    parser.add_argument("--eval_interval", type=int, default=10000)
+
+    # Get parameter dictionary
+    args = vars(parser.parse_args())
+    env = create_env(**args)
+    new_args = init_args(env, **{**args, "env_id": args["safe_rl_env_id"]})
+    args = {**new_args, "env_id": args["env_id"]}
+
+    # Steps to start training
+    alg = create_alg(**args)
+    sampler = create_sampler(**args)
+    buffer = create_buffer(**args)
+    evaluator = create_evaluator(**args, constraint=True)
+    trainer = create_trainer(alg, sampler, buffer, evaluator, **args)
+    trainer.train()
---
+++ gops/algorithm/sac_pen_sep.py
@@ -0,0 +1,198 @@
+#  Copyright (c). All Rights Reserved.
+#  General Optimal control Problem Solver (GOPS)
+#  Intelligent Driving Lab (iDLab), Tsinghua University
+#
+#  Creator: iDLab
+#  Lab Leader: Prof. Shengbo Eben Li
+#  Email: lisb04@gmail.com
+
+__all__ = ["ApproxContainer", "SACPenSep"]
+
+import math
+import time
+from copy import deepcopy
+from typing import Any, Optional
+
+import torch
+import torch.nn as nn
+from torch.optim import Adam
+from gops.algorithm.base import AlgorithmBase
+from gops.algorithm.sac import ApproxContainer as SACApproxContainer, SAC
+from gops.create_pkg.create_apprfunc import create_apprfunc
+from gops.utils.common_utils import get_apprfunc_dict
+from gops.utils.gops_typing import DataDict
+from gops.utils.math_utils import incremental_update
+from gops.utils.tensorboard_setup import tb_tags
+
+
+class ApproxContainer(SACApproxContainer):
+    def __init__(
+        self,
+        value_learning_rate: float,
+        feasibility_learning_rate: float,
+        policy_learning_rate: float,
+        alpha_learning_rate: float,
+        **kwargs,
+    ):
+        super().__init__(
+            value_learning_rate=value_learning_rate,
+            policy_learning_rate=policy_learning_rate,
+            alpha_learning_rate=alpha_learning_rate,
+            **kwargs,
+        )
+        # create feasibility networks
+        g_args = get_apprfunc_dict("feasibility", **kwargs)
+        self.g1: nn.Module = create_apprfunc(**g_args)
+        self.g2: nn.Module = create_apprfunc(**g_args)
+        self.g1_target: nn.Module = deepcopy(self.g1)
+        self.g2_target: nn.Module = deepcopy(self.g2)
+
+        for p in self.g1_target.parameters():
+            p.requires_grad = False
+        for p in self.g2_target.parameters():
+            p.requires_grad = False
+
+        self.g1_optimizer = Adam(self.g1.parameters(), lr=feasibility_learning_rate)
+        self.g2_optimizer = Adam(self.g2.parameters(), lr=feasibility_learning_rate)
+
+
+class SACPenSep(SAC):
+    def __init__(
+        self,
+        index: int = 0,
+        gamma: float = 0.99,
+        tau: float = 0.005,
+        alpha: float = 1.,
+        auto_alpha: bool = True,
+        target_entropy: Optional[float] = None,
+        gamma_g: float = 0.99,
+        penalty: float = 1.,
+        **kwargs: Any,
+    ):
+        AlgorithmBase.__init__(self, index, **kwargs)
+        self.networks = ApproxContainer(**kwargs)
+        self.networks.log_alpha.data.fill_(math.log(alpha))
+        self.gamma = gamma
+        self.tau = tau
+        self.auto_alpha = auto_alpha
+        if target_entropy is None:
+            self.target_entropy = -kwargs["action_dim"]
+        else:
+            self.target_entropy = target_entropy
+        self.gamma_g = gamma_g
+        self.penalty = penalty
+
+    @property
+    def adjustable_parameters(self):
+        return super().adjustable_parameters + ("gamma_g", "penalty")
+
+    def _compute_gradient(self, data: DataDict, iteration: int):
+        start_time = time.time()
+
+        obs = data["obs"]
+        logits = self.networks.policy(obs)
+        act_dist = self.networks.create_action_distributions(logits)
+        new_act, new_logp = act_dist.rsample()
+        data.update({"new_act": new_act, "new_logp": new_logp})
+
+        self.networks.q1_optimizer.zero_grad()
+        self.networks.q2_optimizer.zero_grad()
+        loss_q, q1, q2 = self._compute_loss_q(data)
+        loss_q.backward()
+
+        self.networks.g1_optimizer.zero_grad()
+        self.networks.g2_optimizer.zero_grad()
+        loss_g, g1, g2 = self._compute_loss_g(data)
+        loss_g.backward()
+
+        for p in self.networks.q1.parameters():
+            p.requires_grad = False
+        for p in self.networks.q2.parameters():
+            p.requires_grad = False
+        for p in self.networks.g1.parameters():
+            p.requires_grad = False
+        for p in self.networks.g2.parameters():
+            p.requires_grad = False
+
+        self.networks.policy_optimizer.zero_grad()
+        loss_policy, entropy = self._compute_loss_policy(data)
+        loss_policy.backward()
+
+        for p in self.networks.q1.parameters():
+            p.requires_grad = True
+        for p in self.networks.q2.parameters():
+            p.requires_grad = True
+        for p in self.networks.g1.parameters():
+            p.requires_grad = True
+        for p in self.networks.g2.parameters():
+            p.requires_grad = True
+
+        if self.auto_alpha:
+            self.networks.alpha_optimizer.zero_grad()
+            loss_alpha = -self.networks.log_alpha * (self.target_entropy - entropy)
+            loss_alpha.backward()
+
+        tb_info = {
+            tb_tags["loss_critic"]: loss_q.item(),
+            tb_tags["loss_actor"]: loss_policy.item(),
+            "Loss/Scenery loss-RL iter": loss_g.item(),
+            "SACPen/critic_avg_q1-RL iter": q1.item(),
+            "SACPen/critic_avg_q2-RL iter": q2.item(),
+            "SACPen/scenery_avg_g1-RL iter": g1.item(),
+            "SACPen/scenery_avg_g2-RL iter": g2.item(),
+            "SACPen/entropy-RL iter": entropy.item(),
+            "SACPen/alpha-RL iter": self.alpha.item(),
+            "SACPen/violation-RL iter": data["next_constraint"].mean().item(),
+            tb_tags["alg_time"]: (time.time() - start_time) * 1000,
+        }
+
+        return tb_info
+
+    def _compute_loss_g(self, data: DataDict):
+        obs, act, constraint, obs2, done = (
+            data["obs"],
+            data["act"],
+            data["next_constraint"],
+            data["obs2"],
+            data["done"],
+        )
+        g1 = self.networks.g1(obs, act)
+        g2 = self.networks.g2(obs, act)
+        with torch.no_grad():
+            next_logits = self.networks.policy(obs2)
+            next_act_dist = self.networks.create_action_distributions(next_logits)
+            next_act, _ = next_act_dist.sample()
+            next_g1 = self.networks.g1_target(obs2, next_act)
+            next_g2 = self.networks.g2_target(obs2, next_act)
+            next_g = torch.max(next_g1, next_g2)
+            backup = constraint + (1 - done) * self.gamma_g * next_g
+        loss_g1 = ((g1 - backup) ** 2).mean()
+        loss_g2 = ((g2 - backup) ** 2).mean()
+        return loss_g1 + loss_g2, g1.mean().detach(), g2.mean().detach()
+
+    def _compute_loss_policy(self, data: DataDict):
+        obs, new_act, new_logp = (
+            data["obs"],
+            data["new_act"],
+            data["new_logp"],
+        )
+
+        g1 = self.networks.g1(obs, new_act)
+        g2 = self.networks.g2(obs, new_act)
+        g = torch.max(g1, g2)
+
+        q1 = self.networks.q1(obs, new_act)
+        q2 = self.networks.q2(obs, new_act)
+        q = torch.min(q1, q2)
+
+        loss = (self.alpha * new_logp - q + self.penalty * g).mean()
+        return loss, -new_logp.mean().detach()
+
+    def _update(self, iteration: int):
+        super()._update(iteration)
+
+        self.networks.g1_optimizer.step()
+        self.networks.g2_optimizer.step()
+
+        incremental_update(self.networks.g1, self.networks.g1_target, self.tau)
+        incremental_update(self.networks.g2, self.networks.g2_target, self.tau)
